7911243be3cc51f7925939a8667e692527a50148
Kyle Wyche <KyleW@dhcp-10-245-134-210.fas.harvard.edu>
2018-06-20 16:58:34 -0400

    Update with tweeting text status or figure status

--- pythonScripts/twitterbot.py
+++ pythonScripts/twitterbot.py
@@ -1,16 +1,15 @@
-messages = open('~/GitHub/witnessTree-1/messages/2018-06-15.csv')
+'''set up the messages for tweeting'''
+messages = open('../GitHub/witnessTree-1/messages/2018-06-15.csv')
 message1 = messages.read() #today's message-tdmessage
 messages.close()
 message1
 
-for line in message1:
-    print (line)
-
-import tweepy
-import tkinter
 
 '''Linking the Twitter accound with Tweepy through adding credentials.
 Twitter page _______'''
+
+import tweepy
+import tkinter
 
 consumer_key = 'consumer key'
 consumer_secret = 'consumer secrets'
@@ -26,33 +25,25 @@ api = tweepy.API(auth)
 user = api.me()
 print (user.name)
 
-
-'''Initiate these codes in order to follow-back or unfollow all of the followers of our specific twitter page'''
-
-for follower in tweepy.Cursor(api.followers).items():
-    follower.unfollow()
-    print ("Followed everyone that is following " + user.name)
-
-    
-for follower in tweepy.Cursor(api.followers).items():
-    follower.unfollow()
-    print ("Followed everyone that is following " + user.name)
-
 '''putting a message on Twitter 'Hello World' '''
-
 api = tweepy.API(auth)
-api.update_status('Hello World')
-api.update_with_media(filename[,'it is a bit chilly out'][file])  #How can I write "it's"? Also perhaps where it says chilly I can apply the exercise where based on a temperature the code fills a word
+api.update_status('status')
+api.update_with_media(filename='directory of image', status= 'status')
 
-<<<<<<< HEAD
+'''to delete a status use:'''
 api.destroy_status('tweet ID')# Example: api.destroy_status(1007329170437951488) tweet ID can be found on URL after clicking on the Tweet
-api.me() #gives information about the authenticated user
 
-#These next few lines will print any tweets that were previously tweeted on the account 
+'''These next few lines will print any tweets that were previously tweeted on the account '''
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
                       print(tweet.text)
-=======
-api = tweepy.API(auth)
-api.update_status('Hello World')
->>>>>>> d9423f8c68fe9c27f8d5f211d947c25e30a31621
+
+'''To follow-back or unfollow all of the followers of the specific twitter page'''
+for follower in tweepy.Cursor(api.followers).items():
+    follower.follow()
+    print ("Followed everyone that is following " + user.name)
+
+
+for follower in tweepy.Cursor(api.followers).items():
+    follower.unfollow()
+    print ("unfollowed everyone that is following " + user.name)
