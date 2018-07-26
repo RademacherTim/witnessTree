20d717eee9aec688ae9842a1dcd3dba37e33882f
TTRademacher <rademacher.tim@gmail.com>
2018-07-26 16:55:02 -0400

    started to clean up code in main routine.

--- RScripts/checkEvents.R
+++ RScripts/checkEvents.R
@@ -1,32 +1,68 @@
-'''
-EVENTS: 
-Birthday
-Arbor Day
-Pi Day
-Earth Day
-Spring Eqinox
-Summer Solstice
-Autumn Equinox(needs work)
-Halloween
-Winter Solstice
-New Years
-'''
+#=======================================================================================#
+# Functions to generate messages depending on specific recurring dates such as:
+#
+#       Event                                   Date                 
+#---------------------------------------------------------------------------------------#
+#   1)  New Years                               1st of January       
+#   2)  National Houseplant Appreciation Day    10th of January #TTR Not sure we will have this one
+#   2)  Pi Day                                  14th of March
+#   3)  Birthday                                tbd
+#   4)  Spring Equinox                          20th of March
+#   5)  Earth Day                               22nd of April
+#   6)  Arbor Day                               27th of April
+#   7)  Summer Solstice                         21st of June
+#   8)  Autumn Equinox                          23rd of September
+#   9)  Halloween                               31st of October
+#   10) Winter Solstice                         21st of December
+#
+#---------------------------------------------------------------------------------------#
 
-#Birthday Script (annual post) (change BDay date)
-checkBirthday<-function(mtable){ ## calculate stats for how much witnesstree has grown in a year
-  if (substring(Sys.time(),6,10)=="07-09"){
-    birthdaymessage<-sprintf("Do you know what day it is??? Today is my Birthday! %s",substring(date(),1,10)) # will remove date format from message
-    #calulate priority
-    priority<-1
-    hashtag<-("#It'sMyBirthday")
-    expirDate<-format(Sys.Date(),"%m %d %Y")
-    mtable<-rbind(mtable,c(priority,TRUE,birthdaymessage,hashtag, expirDate))
-  } #else{}
+# New Years (annual post on 1st of January)
+#---------------------------------------------------------------------------------------#
+checkNewYears <- function (mtable) {
+  if (substring (Sys.time (), 6, 10) == '01-01') {
+    newyearsmes <- 'Happy New Years!! I'd join you in celebration but at the moment my roots are quite fixed. What are your plans for the day?''
+    priority    <- 10
+    hashtag     <- '#NewYears'
+    expirDate   <- as.POSIXct (Sys.Date (), format = '%Y-%m-%d')
+    mtable      <- rbind (mtable, c (priority, F, newyearsmes, hashtag, expirDate))
+  } 
+  return (mtable)
+} # TTR To do: - change to include growth in terms of CO2 sequestered of previous year 
+  #              and a new year resolution!
+
+# Pi Day (annual post on 14th of March)
+#---------------------------------------------------------------------------------------#
+checkPiDay <- function (mtable) {
+  if (substring (Sys.time (), 6, 10) == '03-14') {
+    pidaymes  <- c (sprintf ('Happy Pi Day! How many digits of Pi do you know? Tweet below!'),
+                             'Happy Pi Day! Thanks to Pi I can estimate my stem diameter from its girth, if we assume that my stem is a circle.')
+    pidaymes  <- sample (pidaymessage, 1)
+    priority  <- 10
+    hashtag   <- '#PiDay'
+    expirDate <- as.POSIXct (Sys.Date (), format = '%Y-%m-%d')
+    mtable    <- rbind (mtable, c (priority, FALSE, pidaymes, hashtag, expirDate))
+  } 
+  return (mtable)
+} # TTR To do: - find out how to render pi as the greek letter on twitter. 
+  #            - add the diameter and circumference to the second message 
+
+# Birthday 
+#---------------------------------------------------------------------------------------#
+checkBirthday <- function (mtable) { ## calculate stats for how much witnesstree has grown in a year
+  if (substring (Sys.time (), 6, 10) == "07-09") {
+    birthdaymessage <- sprintf ("Do you know what day it is??? Today is my Birthday! %s",substring(date(),1,10)) # will remove date format from message
+    priority  <- 10
+    hashtag   <- ("#It'sMyBirthday")
+    expirDate <- as.POSIXct (Sys.Date (), format = "%Y-%m-%d")
+    mtable    <- rbind (mtable, c (priority, FALSE, birthdaymessage, hashtag, expirDate))
+  }
   return(mtable)
-}
+} # TTR To do: Set birthday (ask John O'Keefe, maybe?)
 
 
-#Arbor Day Script (annual post falls on the last Friday in April)
+# Arbor Day Script (annual post falls on the last Friday in April)
+#---------------------------------------------------------------------------------------#
 checkArborday<-function(mtable){
   if(substring(Sys.time(),6,10)=="04-27"){
   arbordaymessage<-("Happy Arbor day everyone! Did you plant a tree today? Share your comments below")
@@ -39,8 +75,8 @@ checkArborday<-function(mtable){
   return(mtable)
 }
 
-
 #Earth Day Script (annual post)
+#---------------------------------------------------------------------------------------#
 checkEarthday<-function(mtable){
   if(substring(Sys.time(),6,10)=="04-22"){
     earthdaymessage<-("Happy Earth Day! Let's all plant a Tree")
@@ -55,6 +91,7 @@ checkEarthday<-function(mtable){
 
 
 #Spring Equinox (annual post) 
+#---------------------------------------------------------------------------------------#
 checkSpringequinox<-function(mtable){
   if(substring(Sys.time(),6,10)=="03-20"){ # Time needs to be adjusted to account for annual shifts in equinox date
     springequinoxmessage<-("It's the first day of spring! Get ready for bulking season!!")
@@ -69,6 +106,7 @@ checkSpringequinox<-function(mtable){
 
 
 #Autumn Equinox (annual post)
+#---------------------------------------------------------------------------------------#
 checkAutumnequinox<-function(mtable){
   if(substring(Sys.time(),6,10)=="09-23"){ # Date needs to be adjusted to account for annual shifts in equinox date
     autumnequinoxmessage<-("It's the last day of summer and the first of autumn! My trunk has grown ______ this season!!")
@@ -82,6 +120,7 @@ checkAutumnequinox<-function(mtable){
 }
 
 #Summer Solstice (annual post)
+#---------------------------------------------------------------------------------------#
 checkSummersolstice<-function(mtable){
   if(substring(Sys.time(),6,10)=="06-21"){
     summersolsticemessage<-("Soak up that sun and photosynthesis! It's the longest day of the year!")
@@ -96,6 +135,7 @@ checkSummersolstice<-function(mtable){
 
 
 #Winter Solstices (annual post)
+#---------------------------------------------------------------------------------------#
 checkWintersolstice<-function(mtable){
   if(substring(Sys.time(),6,10)=="12-21"){
     wintersolsticemessage<-c("Shortest day of the year! Not too much sunlight today so get as much as you can!", 
@@ -113,23 +153,8 @@ checkWintersolstice<-function(mtable){
   return(mtable)
 }
 
-
-#Pi Day (annual post)
-checkPiday<-function(mtable){
-  if(substring(Sys.time(),6,10)=="03-14"){
-    pidaymessage<-("Happy Pi Day! How many digits of Pi do you know? Tweet below!")
-    #selectedMessage<-sample(pidaymessage,1)
-    #calulate priority
-    priority<-1
-    hashtag<-("#PiDay")
-    expirDate<-format(Sys.Date(),"%m %d %Y")
-    mtable<-rbind(mtable,c(priority,TRUE,pidaymessage,hashtag,expirDate))
-  } 
-  return(mtable)
-}
-
-
 #Halloween (annual post)
+#---------------------------------------------------------------------------------------#
 checkHalloween<-function(mtable){
   if(substring(Sys.time(),6,10)=="10-31"){
     halloweenmessage<-("Happy Halloween! What's your costume? Tweet below!")
@@ -140,21 +165,6 @@ checkHalloween<-function(mtable){
     hashtag<-("#Halloween%s!")
     expirDate<-format(Sys.Date(),"%m %d %Y")
     mtable<-rbind(mtable,c(priority,TRUE,halloweenmessage,hashtag,expirDate))
-  } 
-  return(mtable)
-}
-
-#New Years (annual post)
-checkNewyears<-function(mtable){
-  if(substring(Sys.time(),6,10)=="01-01"){
-    newyearsmessage<-("Happy New Years!! I'd join you in celebration but at the moment my roots are quite fixed. What are your plans for the day?")
-    #selectedMessage<-sample(pidaymessage,1)
-    #calulate priority
-    #figure<- Halloween related image
-    priority<-1
-    hashtag<-("#NewYears")
-    expirDate<-format(Sys.Date(),"%m %d %Y")
-    mtable<-rbind(mtable,c(priority,TRUE,newyearsmessage,hashtag,expirDate))
   } 
   return(mtable)
 }
--- RScripts/witnessTree.R
+++ RScripts/witnessTree.R
@@ -28,12 +28,13 @@ setwd ('../')
 #---------------------------------------------------------------------------------------#
 if (file.exists ('./messages/messages.csv')) {
   messages <- read_csv ('./messages/messages.csv', header = T)
-} else {
-  messages <- tibble (priority = 0, 
-                      fFigure  = F,
-                      message  = '',
-                      hashtags = '', 
-                      expires  = as.POSIXct (Sys.time ()) - 10e9)
+} else { # create a tibble for messages 
+  messages <- tibble (priority = 0,  # priority of message to be posted (int; 
+                                     # between 0 for low and 10 for highest)
+                      fFigure  = F,  # whether it comes with a figure (logical)
+                      message  = '', # the message itself (char) 
+                      hashtags = '', # hastags going with the message (char)
+                      expires  = as.POSIXct (Sys.time ()) - 10e9) # expiration date of the message
   names (messages) <- c ('priority','fFigure','message','hashtags','expires')
 }
 
@@ -47,20 +48,28 @@ messages <- reEvaluatePriorityOf (messages)
 
 # Generate new messages concerning regularly recurrent events
 #---------------------------------------------------------------------------------------#
-messages <- checkBirthday       (messages)
-messages <- checkArborDay       (messages)
-messages <- checkEarthDay       (messages)
-messages <- checkSpringEquinox  (messages)
-messages <- checkAutumnEquinox  (messages)
-messages <- checkSummerSolstice (messages)
-messages <- checkWinterSolstice (messages)
-messages <- checkPiday          (messages)
-messages <- checkHalloween      (messages)
-messages <- checkNewYears       (messages)
+messages <- checkNewYears       (messages) # 01-01
+messages <- checkPiDay          (messages) # 14-03
+messages <- checkBirthday       (messages) # 
+messages <- checkArborDay       (messages) # 
+messages <- checkEarthDay       (messages) # 
+messages <- checkSpringEquinox  (messages) # 
+messages <- checkAutumnEquinox  (messages) # 
+messages <- checkSummerSolstice (messages) # 
+messages <- checkWinterSolstice (messages) # 
+messages <- checkHalloween      (messages) # 
+
+
+# Generate new messages concerning phenology
+#---------------------------------------------------------------------------------------#
+messages <- startOfGrowingSeason (messages)
+messages <- endOfGrowingSeason   (messages)
 
 # Generate new messages concerning climatic events
 #---------------------------------------------------------------------------------------#
-
+#messages <- checkFrost    (messages) # TTR Does not exist yet
+#messages <- checkHeatwave (messages) # TTR Does not exist yet
+ 
 # Selection of message, figure and images for the current iterations
 #---------------------------------------------------------------------------------------#
 message <- selectMessage (messages)
@@ -83,6 +92,6 @@ write_csv (x    = messages,
 # Create log files
 #---------------------------------------------------------------------------------------#
 write_csv (x = sprintf ('%s', format (Sys.time (), "%Y-%m-%d %H:%M")),
-             file = './tmp/logfile.csv')
+             file = './messages/logfile.csv')
 
 #=======================================================================================#
