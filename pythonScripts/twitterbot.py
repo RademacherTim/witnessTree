f6607e9e4e21afd83252fc44a53f3e6024b8805e
TTRademacher <rademacher.tim@gmail.com>
2018-10-05 09:15:32 -0400

    Slowly working up the twitterbot.py to actually connect to the associated account.

--- pythonScripts/twitterbot.py
+++ pythonScripts/twitterbot.py
@@ -1,49 +1,86 @@
-'''set up the messages for tweeting'''
-messages = open('../GitHub/witnessTree-1/messages/2018-06-15.csv')
-message1 = messages.read() #today's message-tdmessage
-messages.close()
-message1
+#==============================================================================#
+# This script is the twitterbot. It reads messages and associated information 
+# to transfer it to a linked twitter account with access details for the account
+# in the config file.
+#------------------------------------------------------------------------------#
+
+# Import dependencies
+#------------------------------------------------------------------------------#
+import sys        # library to use command line arguments
+import tweepy     # 
+import Tkinter    #
+import csv        # for csv handling
 
 
-'''Linking the Twitter accound with Tweepy through adding credentials.
-Twitter page _______'''
+# Read message TTR Need to include checking for a message in the first place
+#------------------------------------------------------------------------------#
+fileName = './messages/2018-10-05_08.csv'
+with open(fileName) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            #print(row))
+            line_count += 1
+        else:
+            #print(row)
+            line_count += 1
+    #print(line_count)
+priority = row [0]
+fFigure = row [1]
+message = row [2]
+hastags = row [3]
+expires = [4]
+print (message)
 
-import tweepy
-import tkinter
 
-consumer_key = 'consumer key'
-consumer_secret = 'consumer secrets'
-access_token = 'access token'
-access_token_secret = 'access token secret'
 
-'''Following lines authenticate the twitter page with tweepy'''
+# Linking the Twitter accound with Tweepy through adding credentials. 
+# Twitter handle @awitnesstree
+#------------------------------------------------------------------------------#
+consumer_key        = sys.argv [1] # consumer key
+consumer_secret     = sys.argv [2] # consumer secrets
+access_token        = sys.argv [3] # access token
+access_token_secret = sys.argv [4] # access token secret
+#print (consumer_key)
+#print (consumer_secret)
+#print (access_token)
+#print (access_token_secret)
+
+# Authenticate the twitter page with tweepy library
+#------------------------------------------------------------------------------#
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-'''following lines are check points to make sure tweepy is linked to the appropriate twitter page'''
+# Check points to make sure tweepy is linked to the appropriate twitter page
+#------------------------------------------------------------------------------#
 user = api.me()
-print (user.name)
+#print (user.name)
 
-'''putting a message on Twitter 'Hello World' '''
+# Putting a message on Twitter
+#------------------------------------------------------------------------------#
 api = tweepy.API(auth)
-api.update_status('status')
-api.update_with_media(filename='directory of image', status= 'status')
+api.update_status(message) # TTR Originally this read 'status' instead of message, but I was trying whether the actually message has to be added here before I ran out of time.
+#api.update_with_media(filename='directory of image', status= 'status')
 
-'''to delete a status use:'''
-api.destroy_status('tweet ID')# Example: api.destroy_status(1007329170437951488) tweet ID can be found on URL after clicking on the Tweet
+# To delete a status use:'''
+#------------------------------------------------------------------------------#
+#api.destroy_status('tweet ID')# Example: api.destroy_status(1007329170437951488) tweet ID can be found on URL after clicking on the Tweet
 
-'''These next few lines will print any tweets that were previously tweeted on the account '''
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-                      print(tweet.text)
+# These next few lines will print any tweets that were previously tweeted on the account '''
+#------------------------------------------------------------------------------#
+#public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#                      print(tweet.text)
 
-'''To follow-back or unfollow all of the followers of the specific twitter page'''
-for follower in tweepy.Cursor(api.followers).items():
-    follower.follow()
-    print ("Followed everyone that is following " + user.name)
+# To follow-back or unfollow all of the followers of the specific twitter page'''
+#------------------------------------------------------------------------------#
+#for follower in tweepy.Cursor(api.followers).items():
+#    follower.follow()
+#    print ("Followed everyone that is following " + user.name)
 
 
-for follower in tweepy.Cursor(api.followers).items():
-    follower.unfollow()
-    print ("unfollowed everyone that is following " + user.name)
+#for follower in tweepy.Cursor(api.followers).items():
+#    follower.unfollow()
+#    print ("unfollowed everyone that is following " + user.name)
--- run.sh
+++ run.sh
@@ -6,6 +6,9 @@ source config
 # Run the witnessTree RScript to generate messages
 Rscript ${WITNESSTREEPATH}RScripts/witnessTree.R
 
+# Run twitterbot to post generated messages
+python pythonScripts/twitterbot.py ${consumer_key} ${consumer_secret} ${access_token} ${access_token_secret}
+
 # Write time and date into log file in the tmp/ folder
 DATE=$(date +%Y-%m-%d" "%H:%M:%S)
 echo ${DATE} >> tmp/logfile.txt
